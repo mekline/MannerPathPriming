11ebcfa3d46c63769270263086c5c83a58d5911b
Annelot de Rechteren van Hemert <AnneCharlotte@AC.local>
2015-10-08 19:25:00 -0400

    noun training

---
+++ HelperFunctions/Noun_Training.m
@@ -0,0 +1,92 @@
+function [] = Noun_Training()
+
+global parameters
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+        % 2 TRIALS OF NOUN TRAINING                             
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+        %%%%%%%%%%%%%%%%%%%%%%
+        %FIRST NOUN TRAINING     
+        %%%%%%%%%%%%%%%%%%%%%%
+        Play_Sound('Audio_stimuli_creation/Finished/aa_nouns/bone1.wav', 'toBlock');
+        Show_Blank;
+
+        movietoplay_noun_2a = 'Movies/2_noun_2a.mov';
+        movietoplay_noun_2b = 'Movies/2_noun_2b.mov';    
+        movietoplay_noun_2_distr = 'Movies/2_noun_2_distractor.mov';
+        Show_Blank;
+
+        PlayCenterMovie(movietoplay_noun_2a);
+
+        Show_Blank;
+        PlaySideMovies(movietoplay_noun_2_distr,'','caption_left','A');
+        Show_Blank;
+        PlaySideMovies('',movietoplay_noun_2b,'caption_right','B'); 
+        Show_Blank;
+
+        Play_Sound('Audio_stimuli_creation/Finished/aa_nouns/bone3.wav', 'toBlock');
+        Show_Blank;   
+        response = Take_Response();
+
+        Play_Sound('Audio_stimuli_creation/Finished/aa_motivation/goodjob.wav', 'toBlock');
+        Show_Blank; 
+
+        starimagenoun1 = 'stars/stars.002.jpg'
+
+        imageArray = imread(starimagenoun1);
+
+        rect = parameters.scr.rect
+
+        winPtr = parameters.scr.winPtr;
+
+        Screen('PutImage', winPtr , imageArray, rect );
+
+        Screen('flip',winPtr)
+        resp = Take_Response();
+        Show_Blank; 
+        
+        %%%%%%%%%%%%%%%%%%%%%%
+        %SECOND NOUN TRAINING     
+        %%%%%%%%%%%%%%%%%%%%%%  
+        
+        Play_Sound('Audio_stimuli_creation/Finished/aa_nouns/glorfin1.wav', 'toBlock');
+        Show_Blank;
+
+        movietoplay_target = 'Movies/1_noun_1_distractor.mov';
+        movietoplay_distractor = 'Movies/1_noun_1b.mov';    
+        movietoplay_sign = 'Movies/1_noun_1a.mov';
+
+        PlayCenterMovie(movietoplay_sign);
+        Show_Blank;
+
+        PlaySideMovies(movietoplay_target,'','caption_left','Z');
+        Show_Blank;
+        PlaySideMovies('',movietoplay_distractor,'caption_right','X'); 
+        Show_Blank;
+
+        Play_Sound('Audio_stimuli_creation/Finished/aa_nouns/glorfin3.wav', 'toBlock');
+        Show_Blank;      
+        Take_Response(); 
+
+        Play_Sound('Audio_stimuli_creation/Finished/aa_motivation/goodjob.wav', 'toBlock');
+        Show_Blank; 
+
+        starimagenoun2 = 'stars/stars.003.jpg'
+
+        imageArray = imread(starimagenoun2);
+
+        rect = parameters.scr.rect
+
+        winPtr = parameters.scr.winPtr;
+
+        Screen('PutImage', winPtr , imageArray, rect );
+
+        Screen('flip',winPtr)
+        resp = Take_Response();
+        Show_Blank;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+    % END NOUN TRAINING                             
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
