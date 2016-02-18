4f0242a81fc9122503ece41d984f7898b0ade7ed
mekline <melissa.e.kline@gmail.com>
2016-02-18 03:04:00 -0500

    fairly extensive monkeying with graphs!

--- Analysis/scripts/MPP_graphs.R
+++ Analysis/scripts/MPP_graphs.R
@@ -1,35 +1,53 @@
 #set path
 setwd("/Users/mekline/Dropbox/_Projects/PrimingMannerPath/MannerPathPriming/Analysis/")
-data = read.csv('all.csv', sep = ",", header = T)
 
 #load libraries
 library(ggplot2)
 
+#CLear history!!!
+rm(list=ls())
+
 #load data
-data = read.csv('all.csv', sep = ",", header = T)
+data = read.csv('all_ext.csv', sep = ",", header = T)
+doExtension = 1
+
+#Drop participants
+data <- data[!is.na(data$INCDECISION),]
+data <- data[data$INCDECISION == 1,]
+length(unique(data$SUBNUM))
 
 #new variables
 data$pathTEST[data$TEST == 'PATHBIAS'] = 1
 data$pathTEST[data$TEST == 'MANNERBIAS'] = 0
 data$pathBIAS[data$BIAS == 'PATHBIAS'] = 1
 data$pathBIAS[data$BIAS == 'MANNERBIAS'] = 0
+data$pathEXT[data$EXT == 'PATHBIAS'] = 1
+data$pathEXT[data$EXT == 'MANNERBIAS'] = 0
 
+dim(data)
 #Remove NA levels (really elsewhere we should document what happened to those lines!)
 data <- data[!is.na(data$EXPERIMENT), ]
 data <- data[!is.na(data$TRIAL), ]
+if (doExtension){
+  data <- data[!is.na(data$EXT),]
+}else{
+  data <- data
+}
 
-#Debugging the binomial testing
+dim(data)
+# #Debugging the binomial testing
 # for (cond in unique(data$EXPERIMENT))
 # {
 #   for (trial in unique(data$TRIAL))
 #   {
-#     n = length(data[data$EXPERIMENT == cond & data$TRIAL == trial,]$pathBIAS)
-#     x = sum(data[data$EXPERIMENT == cond & data$TRIAL == trial,]$pathBIAS, na.rm=TRUE)
+#     n = length(data[data$EXPERIMENT == cond & data$TRIAL == trial,]$pathEXT)
+#     x = sum(data[data$EXPERIMENT == cond & data$TRIAL == trial,]$pathEXT, na.rm=TRUE)
 #     test = prop.test(x, n, conf.level=0.95)
 #     #plotData$intLower[plotData$cond == cond & plotData$verb == trial] = test$conf.int[1]
 #     #plotData$intUpper[plotData$cond == cond & plotData$verb == trial]  = test$conf.int[2]
 #     print('set')
-#     print(x/n)
+#     print(x)
+#     print(n)
 #     print(test$conf.int[1])
 #     print(test$conf.int[2])
 #   }
@@ -51,13 +69,13 @@ makePlot = function(ydata, title="")
         test = prop.test(x, n, conf.level=0.95)
         plotData$intLower[plotData$cond == cond & plotData$verb == trial] = test$conf.int[1]
         plotData$intUpper[plotData$cond == cond & plotData$verb == trial]  = test$conf.int[2]
-      print(test$conf.int[1])
-      print(test$conf.int[2])
+      #print(test$conf.int[1])
+      #print(test$conf.int[2])
       #print(n)
       #print(x)
       }
     }
-    
+    print(plotData)
     #make a plot with ggplot
     pd <- position_dodge(0.1)
     
@@ -66,11 +84,70 @@ makePlot = function(ydata, title="")
       geom_line(position=pd) +
       ylab("Proportion of Path Responses") +
       geom_point(position=pd, size=3) +
-      scale_colour_manual(values = c("green","red"),
-                          name="",
-                          labels=c("Manner", "Path")) +
+      coord_cartesian(ylim=c(0,1)) +
+      #scale_colour_manual(values = c("green","red"),
+                          #name="",
+                          #labels=c("Manner", "Path")) +
     ggtitle(title)
 }
 
 makePlot(data$pathTEST, "Final Test")
 makePlot(data$pathBIAS, "Bias Test")
+makePlot(data$pathEXT, "Cross domain Bias")
+
+#Now let's make some bar graphs too!
+makeBar = function(ydata, title="")
+{
+  plotData = NULL
+  #Aggregate mean/condition
+  plotData = aggregate(x = list(pathmean = ydata), by = list( cond = data$EXPERIMENT), FUN = mean)
+  #get the binomial conf.intervals per condition per trial
+  for (cond in unique(data$EXPERIMENT))
+  {
+    n = length(ydata[data$EXPERIMENT == cond])
+    x = sum(ydata[data$EXPERIMENT == cond])
+    test = prop.test(x, n, conf.level=0.95)
+    
+    plotData$intLower[plotData$cond == cond] = test$conf.int[1]
+    plotData$intUpper[plotData$cond == cond] = test$conf.int[2]
+    
+    
+  }
+  print(plotData)
+  ggplot(plotData, aes(x=cond, y=pathmean, fill=cond)) + 
+    geom_bar(stat="identity") +
+    geom_errorbar(aes(ymin=intLower, ymax=intUpper), colour="black", width=.1) +
+    coord_cartesian(ylim=c(0,1))
+}
+
+makeBar(data$pathTEST, 'foo')
+makeBar(data$pathBIAS, 'foo')
+makeBar(data$pathEXT, 'foo')
+
+
+#Now let's do some stats!
+#How many participants are in this set?
+length(unique(data$SUBNUM))
+#Simplest model: Does EXPERIMENT matter?
+model_eff <- lmer(pathBIAS ~ EXPERIMENT  + (1|SUBNUM), data=data, family="binomial")
+model_noeff <- lmer(pathBIAS ~ 1  + (1|SUBNUM), data=data, family="binomial")
+anova(model_eff, model_noeff)
+
+model_eff <- lmer(pathEXT ~ EXPERIMENT  + (1|SUBNUM), data=data, family="binomial")
+model_noeff <- lmer(pathEXT ~ 1  + (1|SUBNUM), data=data, family="binomial")
+anova(model_eff, model_noeff)
+
+#Do exp and trial interact? That is, is the different between exp different later in the exp? Do they diverge? Yes!
+library(lme4)
+model_inter <- NULL
+model_nointer <- NULL
+model_tonly <- NULL
+model_inter <- lmer(pathBIAS ~ EXPERIMENT*TRIAL  + (1|SUBNUM), data=data, family="binomial")
+model_nointer <- lmer(pathBIAS ~ EXPERIMENT + TRIAL  + (1|SUBNUM), data=data, family="binomial")
+#And considering just the experiment assignment & not time, is there a main effect of experiment? Yes
+model_tonly <- lmer(pathBIAS ~ TRIAL  + (1|SUBNUM), data=data, family="binomial")
+
+anova(model_inter, model_nointer)
+anova(model_nointer, model_tonly)
+
+anova(model_inter, model_tonly)
--- Analysis/scripts/MPP_script_extension.R
+++ Analysis/scripts/MPP_script_extension.R
@@ -9,7 +9,7 @@ nameMetaFile = paste(repodir,"MannerPath_data.csv",sep="")
 #----------------------------------------------------------------------------------
 
 #get all .dat files in the directory
-files = list.files(dir, pattern = ".dat$")
+files = list.files(ddir, pattern = ".dat$")
 #create an empty error vector
 error_files = c()
 #counter to count at which row in the data frame we are
@@ -17,32 +17,35 @@ counter = 1
 #generate an empty data frame for output
 output = data.frame(EXPERIMENT = '', TRIAL = '',BIAS = '',TEST = '', SUBNUM = '', DAYSOLD = '', AGEYEARS = '', INCDECISION = '', EXT = '', stringsAsFactors=F)
 #load the info data file
-#meta = read.csv(nameMetaFile, sep = ",", header = T)
+setwd(adir)
+meta = read.csv(nameMetaFile, sep = ",", header = T)
 #loop over files (participants) and the rows in the file
 setwd(ddir)
 for (j in 1:length(files))
 {
   file = files[j]
-  print(file)
+  #print(file)
   #read in the file
   #If instead of a table we get an error message, move on!
   data = try(read.table(file, sep = ",", header = T, fill=T)) #Fill adds in NAs for blank cells :)
   if (length(data) == 1) {next}
   
   #Hack for the consolidated data folder - if it's a non-extension session, skip it! (And drop kids who didn't complete any trials...)
-  print(dim(data))
   if (dim(data)[2] < 50 || dim(data)[1] < 2) {next}
   
    #test if there is data in that file, else place in the error vector
   if (length(data[,1]) > 0)
   {
     #get info for current participant
-    #info = meta[meta$Participant.. == data$SubjectNo[1],]
+    info = meta[meta$Participant.. == data$SubjectNo[1],]
     for (row in (length(data[,1])-7):length(data[,1]))
     {
       
       #store experiment and trialnumber
-      output[counter,1] = toString(data$Experiment[row])
+      #output[counter,1] = toString(data$Experiment[row])
+      #whoopsy, get it from meta I guess!
+      print(info$Verb.Condition)
+      output[counter,1] = toString(info$Verb.Condition)
       output[counter,2] = data$trialNo[row]
       
       #transform variables into BIAS and TEST
@@ -65,13 +68,13 @@ for (j in 1:length(files))
       
       #Store subject number, days old, age in years and inclusion criteria
       output[counter,5] = data$SubjectNo[row]
-      #output[counter,6] = info$Days.Old
-      #output[counter,7] = info$Age.Years
-      #output[counter,8] = info$Inclusion.Decision
+      output[counter,6] = info$Days.Old
+      output[counter,7] = info$Age.Years
+      output[counter,8] = info$Inclusion.Decision
       #next row
       counter = counter + 1
       
-      if(data$SubjectNo[row] == 54) print(data)
+      #if(data$SubjectNo[row] == 54) print(data)
     }
  }
   else
